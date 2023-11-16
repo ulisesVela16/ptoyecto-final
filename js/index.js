d40c7d33d096f9c4a517f3ce03e2da8d166e4dcc
ulisesVela16 <98621025+ulisesVela16@users.noreply.github.com>
2023-11-16 17:37:30 -0600

    alert

--- js/index.js
+++ js/index.js
@@ -1,45 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const stars = document.querySelectorAll('.star');
-    const ratingValue = document.querySelector('.rating-value');
-    const percentageBar = document.querySelector('.percentage-bar');
-    const percentageText = document.querySelector('.percentage-text');
+    // Agrega la funcionalidad para mostrar la alerta con el enlace del proyecto
+    const verProyectoBtns = document.querySelectorAll('.btn-primary');
 
-    stars.forEach(star => {
-        star.addEventListener('click', () => {
-            const rating = star.getAttribute('data-rating');
-            ratingValue.value = rating;
-            updatePercentage();
-            updatePercentageText();
-            stars.forEach(s => s.classList.remove('active'));
-            star.classList.add('active');
-        });
+    verProyectoBtns.forEach(btn => {
+        btn.addEventListener('click', function () {
+            const enlaceProyecto = btn.getAttribute('href');
+            
+            // Utiliza confirm en lugar de alert
+            const confirmacion = confirm('¿Estás seguro de ver el proyecto?');
 
-        star.addEventListener('mouseover', () => {
-            stars.forEach(s => s.classList.remove('active'));
-            star.classList.add('active');
-        });
-
-        star.addEventListener('mouseout', () => {
-            stars.forEach(s => s.classList.remove('active'));
-            const currentRating = ratingValue.value;
-            const activeStar = document.querySelector(`.star[data-rating="${currentRating}"]`);
-            if (activeStar) {
-                activeStar.classList.add('active');
+            // Verifica la respuesta del usuario
+            if (confirmacion) {
+                // Redirige a la dirección del proyecto si el usuario hace clic en "Aceptar"
+                window.location.href = enlaceProyecto;
+            } else {
+              
             }
-            updatePercentage();
-            updatePercentageText();
         });
     });
-
-    function updatePercentage() {
-        const totalRating = 5;
-        const currentRating = ratingValue.value;
-        const percentage = (currentRating / totalRating) * 100;
-        percentageBar.style.width = `${percentage}%`;
-    }
-
-    function updatePercentageText() {
-        const currentRating = ratingValue.value;
-        percentageText.textContent = `${currentRating * 20}% de usuarios encontraron útil esta página`;
-    }
 });
